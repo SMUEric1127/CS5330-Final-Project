1fa2073717b6242f0475d55327b35d26b67d65ab
SMUEric1127 <85500156+SMUEric1127@users.noreply.github.com>
2023-12-04 15:06:44 -0600

    Add submodule reorganize folders

---
+++ backend/database_source/query.py
@@ -0,0 +1,13 @@
+# Here will be include our custom query function so you can import it in the server
+def exampleQuery():
+    # Your query goes here
+
+    # Return the result
+    return {"student_id": "12345"}
+
+
+def exampleQueryParam(param):
+    # Your query goes here
+
+    # Return the result
+    return {"your param is": param}
