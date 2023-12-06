9226ac5339c54e1c4b2746063ea3a3d4238a872a
SMUEric1127 <85500156+SMUEric1127@users.noreply.github.com>
2023-12-06 06:00:24 -0600

    Update icon image

--- frontend/src/components/images/CircularIcon.js
+++ frontend/src/components/images/CircularIcon.js
@@ -2,8 +2,14 @@ import React from "react";
 
 export const CircularIcon = ({ imageUrl }) => {
   return (
-    <div className="w-11 h-11 rounded-full overflow-hidden">
-      <img src={imageUrl} alt="Icon" className="w-full h-full object-cover" />
+    <div className="rounded-full overflow-hidden">
+      <img
+        src={imageUrl}
+        alt="Icon"
+        className="object-cover"
+        width={40}
+        height={40}
+      />
     </div>
   );
 };
